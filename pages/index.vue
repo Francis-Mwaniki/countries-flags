8ec51b038a6d235ed704ca8070af08129451b662
Francis-Mwaniki <francismwaniki630>
2023-03-27 11:11:50 +0300

    setup

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav>
+      <ul class="flex">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
