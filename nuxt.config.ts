cea4abd19ac1e92b9d7228ff7f144eb92041504b
Francis-Mwaniki <francismwaniki630>
2023-03-29 00:10:46 +0300

    Updated all

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,28 @@ export default defineNuxtConfig({
    modules: ['nuxt-icon','@nuxtjs/tailwindcss','@nuxtjs/color-mode'],
    components: { global: true, dirs: ['~/components'] },
    css: ['~/assets/css/main.css', '~/assets/css/root.css'],
+   app: {
+      head: {
+        charset: "utf-16",
+        title: "Explore continent",
+        meta: [
+          { name: "description", content: "Explore continent" },
+          // { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        ],
+        script: [
+          {
+            src: "https://accounts.google.com/gsi/client",
+            async: true,
+            defer: true,
+          },
+        ],
+        link: [
+          {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Nunito&family=Space+Mono&display=swap",
+          },
+        ],
+      },
+      pageTransition: { name: "page", mode: "out-in" },
+    },
 })
